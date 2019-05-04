4802967ba095979c2a4b7900f3d7fb4c7c472802
Christian.Kohl <christianalbert.k@gmail.com>
2019-05-04 17:28:23 +0100

    Added predicting functionality, untested

--- MLP.py
+++ MLP.py
@@ -2,20 +2,30 @@ import numpy as np
 
 
 class MLP:
-    weights = []
+    layers = []
     learning_rate = 1
 
     def __init__(self, input_size, nodes, output_size, learning_rate):
         if nodes == []:
-            self.weights += Layer([np.random.rand(output_size, input_size+1)])
+            self.layers += Layer([np.random.rand(output_size, input_size+1)])
         else:
-            self.weights += Layer([np.random.rand(nodes[0], input_size+1)])
+            self.layers += [Layer([np.random.rand(nodes[0], input_size+1)])]
             for layer in range(1, len(nodes)):
-                self.weights += Layer([np.random.rand(nodes[layer],
-                                      nodes[layer-1] + 1)])
-            self.weights += Layer([np.random.rand(output_size, nodes[-1]+1)],
-                                  last=True)
+                self.layers += [Layer([np.random.rand(nodes[layer],
+                                      nodes[layer-1] + 1)])]
+            self.layers += [Layer([np.random.rand(output_size, nodes[-1]+1)],
+                                  last=True)]
         self.learning_rate = learning_rate
+
+    def predict(self, inputs):
+        results = []
+        for input in inputs:
+            for idx, layer in enumerate(self.layers):
+                if idx == 0:
+                    transfer = self.layers[idx].transfer(input)
+                else:
+                    transfer = self.layers[idx].transfer(transfer)
+            results += transfer
 
 
 class Layer:
@@ -49,4 +59,4 @@ class Layer:
 
 
 network = MLP(1, [1, 1], 1, 0.8)
-print(network.weights)
+print(network.layers)
